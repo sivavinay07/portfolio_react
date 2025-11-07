f77575f4e851630ffdb6552a304353fd963fb81a
sivavinay <vinaynaidu255@gmail.com>
2025-11-07 11:56:00 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,8 @@ const About = () => {
           enjoy building comprehensive solutions
            from concept to deployment. I craft dynamic and
             responsive user interfaces with React and JavaScript, 
-            while on the backend, I leverage the power of Python, Laravel, and 
-            even C++ to build scalable applications. With a solid foundation in SQL 
+            while on the backend, I leverage the power of Python and even Laravel
+            to build scalable applications. With a solid foundation in SQL 
             databases, I ensure that data is managed effectively. I love creating applications
              that are both powerful and delightful to use.
         </p>
